9ac44747df357b876dabd745aa847f85c88bdd78
jcherniavsky <jcherniavsky@users.noreply.github.com>
2015-03-19 18:03:55 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,37 @@
 
-## The << assignment operator is used to implement cached values of a matrix inversion. The function makeCacheMatrix
-## creates a list of four functions - a get and set function for the incoming array and a get and set function for
-## the cached inverted matrix. Note that for each matrix to be cached, a new invocation of makeCacheMatrix must be made
-## setting new values for the matrix in the calling environment. Similarly, if the array has changed, 
-## a new invocation of makeCacheMatrix must be made. Here is some sample output:
-## 
+## The << assignment operator is used to implement cached values of a matrix inversion. This problem is not fully 
+## specified as, for example, what happens when the function is called with a changed value. cacheSolve has two arguments. The
+## first is a list of four functions - a get and set function for the incoming array and a get and set function for
+## the cached inverted matrix. The second argument is the array to be inverted. The first test is whether the array to be inverted
+## has been modified. If so, the cached values of the set for the incoming array and the set for the cached inverted array are
+## modified to reflect the changed array. If the array was not modified, then a test is made to see if the cache value has to
+## calculated. If no, return the cached value. If yes, calculate the inverted matrix and cache it. Here is some example runs
 ## 
 ## > x <- stats::rnorm(16)
 ## > dim(x) <- c(4,4)
 ## > m <- makeCacheMatrix(x)
-## > cacheSolve(m)
-## [,1]       [,2]       [,3]       [,4]
-## [1,] -0.3378125 -0.9445924 -0.7689950  0.7487606
-## [2,] -3.3116676 -3.2248763 -1.0699306  0.8097309
-## [3,] -0.2663809  0.2229054 -0.3784120 -0.1330681
-## [4,] -1.1471525 -1.3451847 -0.3012276  0.9753661
-## > cacheSolve(m)
+## > cacheSolve(m,x)
+## [,1]       [,2]       [,3]        [,4]
+## [1,]  0.09007438 -0.4531021  0.6151197 -0.03915281
+## [2,]  0.07924133  0.5887923  0.2814162 -0.46584067
+## [3,] -0.19697683 -0.6426029  0.7271492 -0.47339112
+## [4,]  0.38170659  0.2200105 -0.5181898 -0.13789317
+## > y <- cacheSolve(m,x)
 ## getting cached data
+## > cacheSolve(m,y)
+## Cached Matrix was modified
 ## [,1]       [,2]       [,3]       [,4]
-## [1,] -0.3378125 -0.9445924 -0.7689950  0.7487606
-## [2,] -3.3116676 -3.2248763 -1.0699306  0.8097309
-## [3,] -0.2663809  0.2229054 -0.3784120 -0.1330681
-## [4,] -1.1471525 -1.3451847 -0.3012276  0.9753661
-##  
-
-
+## [1,]  2.26613139  0.2092266 -0.8589063  1.5983889
+## [2,] -0.02766335  1.0189113 -0.8177392 -0.6269837
+## [3,]  1.34761267  0.6941942 -0.5737281 -0.7581881
+## [4,]  1.16461128 -0.4038635 -1.5262638 -0.9785987
+## > x
+## [,1]       [,2]       [,3]       [,4]
+## [1,]  2.26613139  0.2092266 -0.8589063  1.5983889
+## [2,] -0.02766335  1.0189113 -0.8177392 -0.6269837
+## [3,]  1.34761267  0.6941942 -0.5737281 -0.7581881
+## [4,]  1.16461128 -0.4038635 -1.5262638 -0.9785987
+##___________________________________________________
 ## This function returns the list of four functions with the "<<-" used to pick the matrix values out of the appropriate
 ## environment (calling environment) when invoked in the cacheSolve function for the two functions that set the values 
 ## of the array and it's inverse.
@@ -49,16 +56,27 @@ makeCacheMatrix <- function(x = matrix()) {m <- NULL
 }
 
 
-## This function checks to see if the matrix had been inverted and cached. If yes, the cached value is returned. If
-## no, the matrix is obtained using get, solved using R's solve function, and stored using setinvert. Note that this
-## function takes the list of functions as input.
+## This function checks two things. First it checks to see if the original matrix had been modifed. It does this by testing using
+## the identical function. If yes, it replaces the old area by the new array, solves the new array, and puts the inverted array in the 
+## cache. It next tests whether the inversion has been yet done - if so, it fetches the cached value. If no, the matrix is inverted
+## and the value put into the cache. Messages are passed to the user if the array has been modifed, and for this assignment, 
+## if cached data is being retrieved.
 
 cacheSolve <- function(x,y, ...) {
-        matrix <- x$get()
-        matrix <- x$getinvert() 
-        if(!is.null(matrix)) {
+        old_matrix <- x$get()
+        
+        if (!identical(old_matrix,y)) {
+                x$set(y)
+                invert <- solve(y, ...)
+                x$setinvert(invert)
+                message("Cached Matrix was modified")
+                return(invert)             
+        }
+        
+        invert <- x$getinvert() 
+        if(!is.null(invert)) {
                 message("getting cached data")
-                return(matrix)
+                return(invert)
         }
         
         matrix <- x$get()
@@ -66,4 +84,3 @@ cacheSolve <- function(x,y, ...) {
         x$setinvert(invert)
         invert
 }
-
